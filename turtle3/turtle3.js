efea17d86fb47db67ae6c99d2f6154ba2e1cb9e4
curved-ruler <pathtrace@protonmail.com>
2025-01-30 14:29:53 +0100

    more presets, circle in 3d

--- turtle3/turtle3.js
+++ turtle3/turtle3.js
@@ -48,16 +48,16 @@ let camera = {
 let progs = [
     `\
 let circ = (a) => {
-    for (let i=0 ; i<360/a; ++i)
+    for (let i=0 ; i<a; ++i)
     {
         T.pen ? T.penup() : T.pendown(); 
         T.forward(1);
-        T.turn([0,0,1],a);
+        T.yaw(360/a);
     }
 };
-circ(3);
-circ(4);
-circ(6);`,
+circ(30);
+circ(40);
+circ(60);`,
 
     `\
 let circ = (a) => {
@@ -65,25 +65,57 @@ let circ = (a) => {
     {
         T.pen ? T.penup() : T.pendown(); 
         T.forward(1);
-        T.turn([0,0,1],360/a);
+        T.yaw(360/a);
     }
 };
 let c = (a) => {
     T.penup();
-    T.turn([0,0,1],-90);
+    T.yaw(-90);
     T.forward(1/(2*Math.sin(Math.PI/a)));
-    T.turn([0,0,1],90);
+    T.yaw(90);
     circ(a);
     T.penup();
-    T.turn([0,0,1],90);
+    T.yaw(90);
     T.forward(1/(2*Math.sin(Math.PI/a)));
-    T.turn([0,0,1],-90);
+    T.yaw(-90);
 }
 
 c(30);
 c(40);
 c(50);
 c(60);`,
+
+
+    `\
+let circ = (a) => {
+    for (let i=0 ; i<a; ++i)
+    {
+        T.pen ? T.penup() : T.pendown(); 
+        T.forward(1);
+        T.yaw(360/a);
+    }
+};
+let c = (a) => {
+    T.penup();
+    T.yaw(-90);
+    T.forward(1/(2*Math.sin(Math.PI/a)));
+    T.yaw(90);
+    circ(a);
+    T.penup();
+    T.yaw(90);
+    T.forward(1/(2*Math.sin(Math.PI/a)));
+    T.yaw(-90);
+}
+
+for (let i=0 ; i<100 ; ++i)
+{
+    T.penup();
+    T.forward(5);
+    T.turn([1,1,1], 10);
+    T.pitch(90);
+    c(i+10);
+    T.pitch(-90);
+}`,
 
 
     `\
@@ -328,6 +360,161 @@ let cube = (d) => {
 };
 
 cube(10);
+`,
+
+
+    `\
+let cube = (d) => {
+    T.penup();
+    T.forward(d/2);
+    T.pitch(-90);
+    T.forward(d/2);
+    T.yaw(90);
+    T.forward(d/2);
+    
+    T.pendown();
+    for (let i=0 ; i<4 ; ++i)
+    {
+        T.pitch(-90);
+        T.forward(d);
+    }
+    
+    T.yaw(90);
+    T.forward(d);
+    T.yaw(90);
+    for (let i=0 ; i<4 ; ++i)
+    {
+        T.forward(d);
+        T.pitch(-90);
+    }
+    
+    T.penup();
+    T.forward(d);
+    T.yaw(90);
+    T.pendown();
+    T.forward(d);
+    
+    T.penup();
+    T.pitch(-90);
+    T.forward(d);
+    T.pendown();
+    T.pitch(-90);
+    T.forward(d);
+    
+    T.penup();
+    T.yaw(90);
+    T.forward(d);
+    T.pendown();
+    T.yaw(90);
+    T.forward(d);
+    
+    T.penup();
+    T.yaw(90);
+    T.forward(d/2);
+    T.yaw(90);
+    T.forward(d/2);
+    T.pitch(-90);
+    T.forward(d/2);
+};
+
+let a = 4;
+for (let i=0 ; i<1000 ; ++i)
+{
+    T.forward(a+2);
+    cube(a);
+    
+    let r = Math.random();
+    let v = r<0.33?[0,0,1]:r<0.66?[0,1,0]:[1,0,0];
+    T.turn(v,(Math.random()<0.5)?90:-90);
+}
+`,
+
+
+    `\
+let cube = (d) => {
+    T.penup();
+    T.forward(d/2);
+    T.pitch(-90);
+    T.forward(d/2);
+    T.yaw(90);
+    T.forward(d/2);
+    
+    T.pendown();
+    for (let i=0 ; i<4 ; ++i)
+    {
+        T.pitch(-90);
+        T.forward(d);
+    }
+    
+    T.yaw(90);
+    T.forward(d);
+    T.yaw(90);
+    for (let i=0 ; i<4 ; ++i)
+    {
+        T.forward(d);
+        T.pitch(-90);
+    }
+    
+    T.penup();
+    T.forward(d);
+    T.yaw(90);
+    T.pendown();
+    T.forward(d);
+    
+    T.penup();
+    T.pitch(-90);
+    T.forward(d);
+    T.pendown();
+    T.pitch(-90);
+    T.forward(d);
+    
+    T.penup();
+    T.yaw(90);
+    T.forward(d);
+    T.pendown();
+    T.yaw(90);
+    T.forward(d);
+    
+    T.penup();
+    T.yaw(90);
+    T.forward(d/2);
+    T.yaw(90);
+    T.forward(d/2);
+    T.pitch(-90);
+    T.forward(d/2);
+};
+
+let go = (x,y,z) => {
+    T.penup();
+    T.forward(x);
+    T.pitch(90);
+    T.forward(z);
+    T.yaw(90);
+    T.forward(y);
+    T.yaw(-90);
+    T.pitch(-90);
+};
+
+let d = 1;
+let gap = 1;
+let n = 7;
+let len = (n-1)*(gap+d);
+go(-len/2,-len/2,-len/2);
+for (let i=0 ; i<n ; ++i)
+{
+    for (let j=0 ; j<n ; ++j)
+    {
+        for (let k=0 ; k<n ; ++k)
+        {
+            cube(d);
+            T.forward(d+gap);
+        }
+        go(-n*(gap+d),gap+d,0);
+    }
+    go(0,-n*(gap+d),gap+d);
+}
+go(0,0,-gap-d);
+go(len/2,len/2,-len/2);
 `
 ];
 let P = null;
